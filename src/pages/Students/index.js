45f7cb7e425b5aacc7f1451f301bdd8ece63f373
FrancioXavier <francioxaviersilva@gmail.com>
2023-11-17 00:14:53 -0300

    Students page consuming api

--- src/pages/Students/index.js
+++ src/pages/Students/index.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container } from '../../styles/GlobalStyles';
+import { Link } from 'react-router-dom';
+import { FaUserCircle, FaEdit, FaWindowClose } from 'react-icons/fa';
 import axios from '../../services/axios';
+import { ProfilePicture, StudentContainer } from './styled';
+import { get } from 'lodash';
 
 export default function Students() {
-  React.useEffect(() => {
+  const [students, setStudent] = useState([]);
+
+  useEffect(() => {
     async function getData() {
       const response = await axios.get('/alunos');
-      console.log(response.data);
+      setStudent(response.data);
     }
 
     getData();
@@ -14,6 +20,30 @@ export default function Students() {
   return (
     <Container>
       <h1>Alunos</h1>
+
+      <StudentContainer>
+        {students.map((student) => (
+          <div key={String(student.id)}>
+            <ProfilePicture>
+              {get(student, 'Fotos[0].url', '') ? (
+                <img src={student.Fotos[0].url} alt="" />
+              ) : (
+                <FaUserCircle size={36} />
+              )}
+            </ProfilePicture>
+
+            <spam>{student.nome} </spam>
+            <spam>{student.email}</spam>
+
+            <Link to={`/student/${student.id}/edit`}>
+              <FaEdit size={16} />
+            </Link>
+            <Link to={`/student/${student.id}/delete`}>
+              <FaWindowClose size={16} />
+            </Link>
+          </div>
+        ))}
+      </StudentContainer>
     </Container>
   );
 }
